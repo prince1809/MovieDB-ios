2f98bd037dd8d3a67283dad6c94b67a2514c5eb0
prince <prince18pat@gmail.com>
2019-09-13 01:11:32 +0900

    Initial Empty project created

---
+++ MovieDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieDB
+//
+//  Created by Prince Kumar on 2019/09/13.
+//  Copyright © 2019 Prince Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
