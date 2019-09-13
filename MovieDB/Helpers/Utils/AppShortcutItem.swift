5d51cb0dc2e586fe440319700e0ac43200808a03
prince <prince18pat@gmail.com>
2019-09-14 00:49:01 +0900

    update storyboard and add controllers

---
+++ MovieDB/Helpers/Utils/AppShortcutItem.swift
@@ -0,0 +1,15 @@
+//
+//  AppShortcutItem.swift
+//  MovieDB
+//
+//  Created by Prince Kumar on 2019/09/14.
+//  Copyright © 2019 Prince Kumar. All rights reserved.
+//
+
+import Foundation
+
+
+
+enum AppShortcutItem: String {
+    case searchMovies
+}
---
+++ MovieDB/Scenes/Splash/SplashViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  SplashViewModel.swift
+//  MovieDB
+//
+//  Created by Prince Kumar on 2019/09/14.
+//  Copyright © 2019 Prince Kumar. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+final class SplashViewModel {
+    
+    
+    
+}
