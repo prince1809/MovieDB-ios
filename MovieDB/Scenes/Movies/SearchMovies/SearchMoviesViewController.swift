7581c83cc0aeb2ee697f3891e5cc1659fd0eea24
prince <prince18pat@gmail.com>
2019-09-14 22:10:58 +0900

    add storyboard reference

---
+++ MovieDB/Scenes/Movies/SearchMovies/SearchMoviesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchMoviesViewController.swift
+//  MovieDB
+//
+//  Created by Prince Kumar on 2019/09/14.
+//  Copyright © 2019 Prince Kumar. All rights reserved.
+//
+
+import UIKit
+
+class SearchMoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MovieDB/Scenes/Movies/UpcomingMovies/UpcomingMoviesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UpcomingMoviesViewController.swift
+//  MovieDB
+//
+//  Created by Prince Kumar on 2019/09/14.
+//  Copyright © 2019 Prince Kumar. All rights reserved.
+//
+
+import UIKit
+
+class UpcomingMoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
