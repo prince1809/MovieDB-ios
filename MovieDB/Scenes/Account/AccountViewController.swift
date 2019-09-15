e9851444c05484eb190942dcf4c8dd244677a5f1
prince <prince18pat@gmail.com>
2019-09-15 19:27:02 +0900

    Add navigation controller, icons and link storyboards

---
+++ MovieDB/Scenes/Account/AccountViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AccountViewController.swift
+//  MovieDB
+//
+//  Created by Prince Kumar on 2019/09/15.
+//  Copyright © 2019 Prince Kumar. All rights reserved.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
